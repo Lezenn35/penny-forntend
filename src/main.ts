682b803620a782ff56ea9345ab203e403659cb46
Lezenn <gireg.lezoraine2022@campus-eni.fr>
2022-04-26 00:38:35 +0200

    P5 init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import './style.css'
+import './style.css';
+import P5 from "p5";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const sketch = (p5: P5) => {
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  p5.setup = () => {
+    p5.createCanvas(window.innerWidth, window.innerHeight);
+  };
+
+  p5.draw = () => {
+    p5.background(100);
+
+    p5.ellipse(window.innerWidth / 2, window.innerHeight / 2, 150, 150);
+  };
+
+};
+
+new P5(sketch);
